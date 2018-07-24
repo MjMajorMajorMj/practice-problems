c489c54d78e6c06a227a361c220bb7053bc6316e
MjMajorMajorMj <techk8@gmail.com>
2018-07-24 10:14:42 -0700

    Finished practice 21

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,5 @@
-answer.js
+function bipolarLoop(firstNum, secondNum, limit) {
+    for (let limitCount = 0; limitCount <=limit; firstNum++, secondNum--, limitCount++) {
+        console.log(firstNum, secondNum);
+    }
+};
